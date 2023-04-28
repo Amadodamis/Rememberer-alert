4f6b6d6e2c5b72935a826e20060db655e912d097
Amado <amadodamis@gmail.com>
2023-04-28 15:15:46 -0300

    retoques front end

--- src/components/ReminderList.jsx
+++ src/components/ReminderList.jsx
@@ -1,38 +1,43 @@
 import "./reminderList.css"
 import editIcon from "../img/edit.png"
-
+import deleteIcon from "../img/delete.png"
 export default function ReminderList() {
 
     let reminderModel = {
         message: "Comprar plasticola para el proyecto de ciencias.",
-        year: "2017",
-        month: "12",
-        day: "2",
-        hour: "11",
-        minute: "21",
+        year: "2023",
+        month: "05",
+        day: "1",
+        hour: "09",
+        minute: "15",
     }
 
-    let array = [reminderModel, reminderModel, reminderModel, reminderModel, reminderModel, reminderModel, reminderModel, reminderModel,]
+    let array = [reminderModel, reminderModel, reminderModel, reminderModel, reminderModel, reminderModel, reminderModel,]
+    //let array =[]
 
     return (
         <section className="box-ReminderList">
             <h2 className="title-list-reminders">
                 List of reminders
             </h2>
+            <div className='container-items'>
 
-            {array.length ?
-                array.map((reminder, i) =>
+                {array.length ?
+                    array.map((reminder, i) =>
 
-                    <div className="item" key={i}>
-                        <p className="text-list message">{reminder.message}</p>
-                        <p className="text-list">{reminder.year}-{reminder.month}-{reminder.day} {reminder.hour}:{reminder.minute}hs</p>
-                        <img src={editIcon} alt='IconEdit.png' className='icon-edit' />
-                    </div>
+                        <div className="item" key={i}>
+                            <p className="text-list message">{reminder.message}</p>
+                            <p className="text-list">{reminder.day}-{reminder.month}-{reminder.year} {reminder.hour}:{reminder.minute}hs</p>
+                            <img src={editIcon} alt='IconEdit.png' className='icon-edit' />
+                        </div>
 
-                )
-                :
-                <p className="text-list" >No hay recordatorios</p>
-            }
+                    )
+                    :
+                    <p className="text-list no-reminder" >No hay recordatorios.,</p>
+                }
+            </div>
+
+            <img src={deleteIcon} alt='delete-icon' className='delete-icon' />
 
         </section>
     )
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Title() {
   return (
     <h2 className='Title-app'>
-        Rememberer app
+        Reminder App
     </h2>
   )
 }
